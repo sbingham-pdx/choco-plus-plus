23dc0d0c3d909b54b459b4305f78140441e395d7
victor <castro.hernandez@yahoo.com>
2022-11-10 20:35:44 -0800

    'Testing the manager terminal w/struct'

---
+++ manager.cpp
@@ -0,0 +1,22 @@
+#ifndef _MANAGER
+# define _MANAGER
+
+//Include Steven's file to access provider struct 
+#include <structures.h>
+using namespace std; 
+
+//Where the program begins 
+int
+main(){
+
+       
+
+
+
+
+
+
+    return 1;
+}
+
+#endif //_MANAGER
