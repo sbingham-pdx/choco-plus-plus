0d771e13b1f97e71e412b4f53cc12f6dfa9a9789
lily-machamer <lily-machamer>
2022-11-15 17:32:35 -0800

    Started building out text input files for provider and main

---
+++ chocoplusplus.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "pterminal.cpp"
+using namespace std; 
+
+int main(){
+
+	int selection = 0;
+    
+	cout << "Welcome to Choco Plus Plus\n";
+	cout << "Please enter an integer for\n(1)Manager, (2)Operator, or (3)Provider terminals\n";
+	
+	cin >> selection;
+
+	switch(selection) {
+		case 1:
+			cout << "Manager Terminal\n";
+			break;
+		case 2:
+			cout << "Operator Terminal\n";
+			break;
+		case 3:
+			cout << "Provider Terminal\n";
+			pterminal();
+			break;
+		default:
+			cout << "Invalid entry, I will die now :(\n";
+			return 2;
+	}
+
+	return 1;
+}
